bedae2a2bd55af8004216181f1f0035df4f79876
SnapDragon7410 <52431319+SnapDragon7410@users.noreply.github.com>
2019-07-15 22:55:38 +0200

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -12,11 +12,9 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = "****:"
-    help_string = """@UniBorg ( **Custom Built By** @r4v4n4 ) \n**Verified Account**: ✅\n**Official Website**: https://ravanaisdrunk.site.live\n
-Pithun {}
-Talethrun {}
+    help_string = """@UniBorg ( **Custom Fork By** @snappy101 ) \n**Verified Account**: ✅\n**\nPython {}\nTelethon {}
 
-**Custom Built Fork**: https://github.com/ravana69/PornHub""".format(
+**Custom Built Fork**: https://github.com/SnapDragon7410/UniBorg""".format(
         sys.version,
         __version__
     )
