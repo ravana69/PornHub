1807f4627bec1ba97b1cd4c145bccd040f6d2d71
keshav12-36 <57794378+keshav12-36@users.noreply.github.com>
2019-11-27 23:57:43 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -14,11 +14,11 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = "****:"
-    help_string = """@UniBorg ( **Custom Built By** @r4v4n4 ) \n**Verified Account**: ✅\n**Official Website**: https://ravanaisdrunk.site.live\n
-Pithun {}
+    help_string = """@UniBorg ( **Custom Built By** @tera_yar_hu_mai ) \n**Verified Account**: ✅\n**Official Website**: https://github.com/python-telegram-bot/python-telegram-bot
+ Pithun {}
 Talethrun {}
  
-**Custom Built Fork**: https://github.com/ravana69/PornHub""".format(
+**Custom Built Fork**: https://youtu.be/bFXs1ffr3Ms""".format(
         sys.version,
         __version__
     )
