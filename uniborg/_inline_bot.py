63568907a788a4c79e9c633ce9beac12eb905051
NobArxtx <54905770+NobArxtx@users.noreply.github.com>
2020-01-19 09:54:51 +0530

    Update _inline_bot.py

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -86,20 +86,20 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             result = builder.article(
                 "© @UniBorg",
-                text="""@r4v4n4 **( Custom Built By** @r4v4n4 **)** 
-**Verified Account:** ✅
-**Official Website:** https://ravanaisdrunk.site.live [⠀](https://telegra.ph/file/b0604ea53360cd3858ec5.mp4)
-
-**Pithun 3.7.4 (default, Sep 12 2019, 01:19:52)** 
+                text="""@Classes_dex **( Custom Built By** @Classes_dex **)** 
+*Website:** https://animeflix.in [⠀](https://telegra.ph/file/b0652363480f3a7c316f6.mp4)
+*Verified Account:** ✅
+**
+**Python 3.7.4 (default, Sep 12 2019, 01:19:52)** 
 **[GCC 7.4.0]**
-**Talethrun 1.10.6**
+**Telethon 1.10.6**
 
-**Custom Built Fork:** https://github.com/ravana69/Pornhub""",
+**Custom Built Fork:** https://github.com/NobArxtx/Newborg""",
                 buttons=[
-                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/r4v4n4"), custom.Button.url(
+                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/Classes_dex"), custom.Button.url(
                         "📼Ravana Audio Memes📼", "https://t.me/tgaudiomemes")],
-                    [custom.Button.url("👨‍💻Source Code👨‍💻", "https://github.com/ravana69/Pornhub"), custom.Button.url(
-                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F&template=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F")],
+                    [custom.Button.url("👨‍💻Source Code👨‍💻", "https://github.com/NobArxtx/Newborg"), custom.Button.url(
+                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FNobArxtx%2FNewborg%2F&template=https%3A%2F%2Fgithub.com%2FNobArxtx%2FNewborg%2F")],
                     [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"), custom.Button.url(
                         "✳️Fork Boost✳️", "tg://some_unsupported_feature"), custom.Button.url(
                         "♻️Refresh Heroku♻️", "tg://chutiya")]
